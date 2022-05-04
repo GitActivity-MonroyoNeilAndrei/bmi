274583bc59600cf50064eaba226b81e6120c9a3b
MonroyoNeilAndrei <andreimonroyo0@gmail.com>
2022-05-04 15:13:53 +0800

    bmi website

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleShit/bmi.css">
+    <title>Design Template</title>
+</head>
+
+<body>
+    <div class="paper">
+        <div class="border">
+            <div style="margin: 0px auto;">
+                <div class="div1">
+                    <h4 style="padding-left: 10px; padding-top: 60px; padding-bottom: 0px; font-family: sans-serif; line-height:16px;"> MAY 9, 2022 NATIONAL AND LOCAL ELECTIONS </h4>
+                    <p style="padding-left: 10px; padding-top: 0px; font-family: sans-serif; font-weight: normal; line-height: 5px;"> BUNGANAY, BOAC, MARINDUQUE </p>
+                    <p style="padding-left: 10px; padding-top: 0px; font-family: sans-serif; font-weight: normal; line-height: 5px;"> Type: National and Local </p>
+                    <pre style="padding-left: 10px; font-weight: bold; font-size: 15px">
+
+INSTRUCTION FOR VOTING
+</pre>
+                    <pre style="padding-left: 10px;" class="putUp">
+(1) Complete blacken the inside of the circle 
+beside the name of the desired candidate.
+(2) Use only the marking pen provided for
+blackening the  circles.
+(3) Do not blacken more circles that what is intended
+                </pre>
+                </div>
+                <div class="div2">
+                    <div>
+                        <img src="images/barcode.jpg" class="barcode">
+                    </div>
+                </div>
+                <div class="div3">
+                    <div class="modify">
+                        <div>
+                            <img src="images/republika-ng-pilipinas.jpg" class="republika-logo">
+                        </div>
+                        <div class="republika2">
+                            <div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p style="text-align: right;">Clustered Precinct ID: 40010002</p>
+                        <p style="text-align: right;">Precints in Cluster:</p>
+                        <p style="text-align: right;">0005A, 0005B, 0006A</p>
+                    </div>
+                    <div class="box">
+                        <center>
+                            <h5 style="margin-top: 15%;">Signiture of Chairman</h5>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <div style=" width: 200px; height: 1000px; border-style: solid; border-width: 10px; background-color: grey; margin-top: 200px;">
+                <h2> andrei </h2>
+                <p> monroyo </p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
